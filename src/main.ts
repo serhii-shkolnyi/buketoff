f1dafdeffe29a24807789d4532f9498a819205f8
serhii-shkolnyi <s.shkolniy@gmail.com>
2023-01-25 17:17:52 +0200

    test migration table Users and connect to database

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,21 @@
 import express from 'express';
+import 'reflect-metadata';
 
 import { apiConfig } from './config';
+import { AppDataSource } from './data-source';
 
 const app = express();
 
-app.listen(apiConfig.PORT, () => {
+app.listen(apiConfig.PORT, async () => {
     console.log(`Server started on PORT: ${apiConfig.PORT}`);
+    try {
+        const connect = await AppDataSource.initialize();
+        if (connect) {
+            console.log('connect to database');
+        }
+    } catch (err) {
+        if (err) {
+            console.log('error connect to database');
+        }
+    }
 });
---
+++ src/migration/1674655070294-CreateTableUsers.ts
@@ -0,0 +1,48 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
+
+export class CreateTableUsers1674655070294 implements MigrationInterface {
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.createTable(
+            new Table({
+                name: 'Users',
+                columns: [
+                    {
+                        name: 'id',
+                        type: 'int',
+                        isPrimary: true,
+                        isGenerated: true,
+                        generationStrategy: 'increment',
+                    },
+                    {
+                        name: 'email',
+                        type: 'varchar',
+                        width: 255,
+                        isUnique: true,
+                        isNullable: false,
+                    },
+                    {
+                        name: 'password',
+                        type: 'varchar',
+                        width: 255,
+                        isNullable: false,
+                    },
+                    {
+                        name: 'createdAt',
+                        type: 'timestamp',
+                        isNullable: false,
+                        default: 'now()',
+                    },
+                    {
+                        name: 'deletedAt',
+                        type: 'timestamp',
+                    },
+                ],
+            }),
+            true,
+        );
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.dropTable('Users', true);
+    }
+}
